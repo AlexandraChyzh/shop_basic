8f7fab99e7f42259ac35b452544d9612b0822d09
AlexandraChyzh <alexandra.25101980@gmail.com>
2024-12-10 21:36:26 +0100

    Controller layer implemented

--- src/main/java/app/controller/CustomerController.java
+++ src/main/java/app/controller/CustomerController.java
@@ -1,4 +1,60 @@
 package app.controller;
 
+import app.domain.Customer;
+import app.exceptions.CustomerNotFoundException;
+import app.exceptions.CustomerSaveException;
+import app.exceptions.CustomerUpdateException;
+import app.service.CustomerService;
+
+import java.util.List;
+
 public class CustomerController {
+
+    private final CustomerService service;
+
+    public CustomerController(CustomerService service) {
+        this.service = service;
+    }
+    public Customer save(String name) throws CustomerSaveException {
+        Customer customer = new Customer(name);
+        return service.save(customer);
+    }
+    private List<Customer> getAllActiveCustomers(){
+        return service.getAllActiveCustomers();
+    }
+    private Customer getActiveCustomerById(Long id) throws CustomerNotFoundException{
+        return service.getActiveCustomerById(id);
+    }
+    private void updateActiveCustomer(Long id, String name) throws CustomerUpdateException, CustomerNotFoundException{
+         Customer customer = new Customer(id, name);
+         service.updateActiveCustomer(customer);
+    }
+    private void deleteById(Long id) throws CustomerNotFoundException{
+         service.deleteById(id);
+    }
+    private void deleteByName(String name){
+         service.deleteByName(name);
+    }
+    private void restoreById(Long id){
+         service.restoreById(id);
+    }
+    private int getActiveCustomersNumber(){
+         return service.getActiveCustomersNumber();
+    }
+    private double getCustomersCartTotalPrice(Long id) throws CustomerNotFoundException{
+         return service.getCustomersCartTotalPrice(id);
+    }
+    private double getCustomersCartAveragePrice(Long id){
+         return service.getCustomersCartAveragePrice(id);
+    }
+    private void addProductToCustomersCart(Long customerId, Long productId){
+         service.addProductToCustomersCart(customerId, productId);
+    }
+    private void deleteProductFromCustomersCart(Long customerId, Long productId){
+         service.deleteProductFromCustomersCart(customerId, productId);
+    }
+    private void clearCustomersCart(Long id){
+         service.clearCustomersCart(id);
+}
+
 }
--- src/main/java/app/controller/ProductController.java
+++ src/main/java/app/controller/ProductController.java
@@ -1,4 +1,53 @@
 package app.controller;
 
+import app.domain.Product;
+import app.exceptions.ProductNotFoundException;
+import app.exceptions.ProductSaveException;
+import app.exceptions.ProductUpdateException;
+import app.service.ProductService;
+
+import java.util.List;
+
 public class ProductController {
+
+    private ProductService service;
+
+
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
+
+   public Product save(String title, double price) throws ProductSaveException{
+        Product product = new Product(title, price);
+        return service.save(product);
+   }
+   public List<Product> getAllActiveProducts(){
+        return service.getAllActiveProducts();
+   }
+   public Product getActiveProductById(Long id) throws ProductNotFoundException{
+        return service.getActiveProductById(id);
+   }
+   public void updateActiveProduct(Long id, double price) throws ProductUpdateException, ProductNotFoundException{
+       Product product = new Product(id, price);
+       service.updateActiveProduct(product);
+
+   }
+   public void deleteById(Long id) throws ProductNotFoundException{
+        service.deleteById(id);
+   }
+   public void deleteByTitle(String title){
+        service.deleteByTitle(title);
+   }
+   public void restoreById(Long id){
+        service.restoreById(id);
+   }
+   public int getActiveProductNumber(){
+        return service.getActiveProductNumber();
+   }
+   public double getActiveProductsTotalCost(){
+        return service.getActiveProductsTotalCost();
+   }
+   public double getActiveProductsAveragePrice(){
+        return service.getActiveProductsAveragePrice();
+   }
 }
